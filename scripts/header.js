4f9d58ca7469fb5ae60db502419ffc14923f3a8d
Joe Bloggs <Jack.Watson3@student.shu.ac.uk>
2023-11-25 22:48:12 +0000

    Changed header to be responsive to device.

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,17 @@
-const divHeaderLogo = document.getElementById("div-logo-in-header");  //Concept for moving https://stackoverflow.com/questions/1363650/javascript-moving-element-in-the-dom
-const divNavigation = document.getElementById("div-navigation");
-const sectMain = document.getElementById("sect-main");
+const divHeaderLogo = document.querySelector(".TARGET");
+const divNavigation = document.querySelector(".MOVEME");  
+const divOriginal = document.querySelector(".ORIGINAL");
 
-divNavigation.before(divHeaderLogo);
-divNavigation.after(sectMain);
+window.addEventListener('resize', resize); //Concept for having an event listener for screen width from https://stackoverflow.com/questions/39557244/run-code-if-screen-has-certain-width
+
+function resize() 
+{
+    if (window.innerWidth < 700) 
+    {
+        divOriginal.appendChild(divNavigation); //Concept for moving div element to another area from https://www.w3schools.com/jsref/met_node_appendchild.asp
+    }
+    else
+    {
+        divHeaderLogo.appendChild(divNavigation);
+    }
+}
