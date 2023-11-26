f7b2cae9580dcd0a8a17bd50b443f88492542301
Joe Bloggs <Jack.Watson3@student.shu.ac.uk>
2023-11-26 03:45:49 +0000

    Improved footer. Spiced up images.

--- scripts/header.js
+++ scripts/header.js
@@ -4,11 +4,20 @@ const divOriginal = document.querySelector(".ORIGINAL");
 
 window.addEventListener('resize', resize); //Concept for having an event listener for screen width from https://stackoverflow.com/questions/39557244/run-code-if-screen-has-certain-width
 
+if (window.innerWidth < 700) 
+    {
+        divOriginal.appendChild(divNavigation); //Concept for moving div element to another area from https://www.w3schools.com/jsref/met_node_appendchild.asp
+    }
+    else
+    {
+        divHeaderLogo.appendChild(divNavigation);
+    }
+
 function resize() 
 {
     if (window.innerWidth < 700) 
     {
-        divOriginal.appendChild(divNavigation); //Concept for moving div element to another area from https://www.w3schools.com/jsref/met_node_appendchild.asp
+        divOriginal.appendChild(divNavigation);
     }
     else
     {
